292d696330e6c549728c8217044ab05c41e2a544
Ahmed Shehryar <raoahmedshehryar2006@gmail.com>
2025-07-16 00:22:21 +0500

    Just the basic project structure and User model for the database

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,22 @@
+
+export const signup = (req, res) => {
+    const {fullName, email, password} = req.body;
+
+    try {
+        
+        
+    } catch (error) {
+        
+    }
+
+}
+
+export const login = (req, res) => {
+
+    
+}
+
+export const logout = (req, res) => {
+
+    
+}
