b7bec7d69ed2964628c98fab7218a978a0d3c053
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-07-17 19:09:05 +0500

    Added jwt to signup in the backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 
 
@@ -26,13 +27,23 @@ export const signup = async (req, res) => {
         })
 
         if(newUser){
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
 
         }else{
             return res.status(400).json({message:"Invalid User data"});
         }
 
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message:"Internal server error"});
     }
 
 }
