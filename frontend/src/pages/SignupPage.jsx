b69b8125f857031fa4d22c11edb40f2330293daf
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-07-24 22:54:25 +0500

    Scaffolded basic Signup logic in the backend

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+ import { useState } from "react"
+import { useAuthStore } from "../store/useAuthStore";
 
 const SignupPage = () => {
+
+  const[showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName:"",
+    email:"",
+    password:"",
+  });
+
+  const {signup, isSigningUp} = useAuthStore();
+
+  const validateForm = () => {
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div>SignupPage</div>
   )
