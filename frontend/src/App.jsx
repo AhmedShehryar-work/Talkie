4ce598823db97c113d4a0502fe3a0c70f2a055c8
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-07-24 22:34:15 +0500

    Added CORS(access to all - will change tha later) + Added Luide-react and Loader + Added some more to the Store
    Also fixed a small bug in message contoller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,26 +6,40 @@ import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 
-import {Routes, Route} from "react-router-dom"
+import {Loader} from "lucide-react"
+
+import {Routes, Route, Navigate} from "react-router-dom"
 import {useAuthStore} from './store/useAuthStore'
+import { useEffect } from 'react'
 
 const App = () => {
 
-  const {authUser} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+console.log(authUser);
 
   return (
-    <div>
+    <>
+    {isCheckingAuth && !authUser && (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className="size-10 animate-spin"/>
+      </div>
+      )}
+
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <Homepage /> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/login"/> } />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/login"/> } />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login"/> } />
       </Routes>
 
-    </div>
+    </>
   )
 }
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,25 @@
 import {create} from "zustand"
+import {axi} from "../lib/axios.js"
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isCheckingAuth: true
+    isSigningUp:false,
+    isLoggingIn:false,
+    isUpdatingProfile: true,
+
+    isCheckingAuth:true,
+
+    checkAuth: async() =>{
+        try {
+            
+            const res = axi.get("/auth/check");
+            set({authUser:res.data});
+
+        } catch (error) {
+            console.log("Error in checkAuth", error)
+            set({authUser:null});
+        }finally{
+            set({isCheckingAuth: false})
+        }
+    }
 }))
