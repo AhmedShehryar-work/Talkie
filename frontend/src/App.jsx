2910f34837a7a75343dfe3587e0063ae69e36d0c
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-07-26 23:49:38 +0500

    Routing logic fixed but the authUser is populated after sometime i.e it is null at first render of routes so all routes are accessible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,8 +36,8 @@ console.log(authUser);
 
       <Routes>
         <Route path="/" element={authUser ? <Homepage /> : <Navigate to="/login"/>} />
-        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/login"/> } />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/login"/> } />
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/"/> } />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"/> } />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login"/> } />
       </Routes>
